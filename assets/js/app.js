35ed61798389e899db37c6e16ebe383f5a39d785
Tharunkunamalla <kunamallatharun701@gmail.com>
2025-03-31 22:57:34 +0530

    done some changes

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,3 +1,10 @@
-document.getElementById("signInBtn").addEventListener("click", function () {
-  window.location.href = "login.html";
+document.addEventListener("DOMContentLoaded", function () {
+  const signInBtn = document.getElementById("signInBtn");
+  if (signInBtn) {
+    signInBtn.addEventListener("click", function () {
+      window.location.href = "login.html";
+    });
+  } else {
+    console.error("Element with ID 'signInBtn' not found.");
+  }
 });
